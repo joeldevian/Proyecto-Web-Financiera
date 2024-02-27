692661037843e3825234cf11b03b8a56e3e59216
creifer <157430850+creifer12@users.noreply.github.com>
2024-02-27 17:52:55 -0500

    corregido

--- conexion/conectar.php
+++ conexion/conectar.php
@@ -3,13 +3,3 @@
 $conex = mysqli_connect("localhost", "root", "", "registro");
 mysqli_set_charset($conex, 'utf8');
 
-
-
-
-// Verificar la conexión
-if (!$conex) {
-    die("Error al conectar con la base de datos: " . mysqli_connect_error());
-} else {
-    echo "Conexión exitosa!";
-}
-?>
